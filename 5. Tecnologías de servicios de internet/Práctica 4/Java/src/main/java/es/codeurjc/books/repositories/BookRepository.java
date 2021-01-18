b8b4fccb543c269f720456020fa97192b1ac0b06
Juan Manuel <sanblas2b.juanma@gmail.com>
2021-01-18 21:11:29 +0100

    Deleted sources

--- 5. Tecnologías de servicios de internet/Práctica 4/Java/src/main/java/es/codeurjc/books/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package es.codeurjc.books.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.codeurjc.books.models.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
--- 5. Tecnologías de servicios de internet/Práctica 4/Node/src/config.js
+++
@@ -1,1 +0,0 @@
-module.exports.SECRET = "KEY_1234";
